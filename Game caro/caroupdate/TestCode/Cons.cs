56aab6e761c4457f0b7321b9e0d446e1fb68abbb
DIEMVO <43290038+DIEMVO@users.noreply.github.com>
2018-11-20 09:49:56 +0700

    update

---
+++ Game caro/caroupdate/TestCode/Cons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCaro
+{
+	// KHởi tạo các hằng số
+	public class Cons
+	{
+		public static int ChieuRongbtn = 30;		//Chiều cao, chiều dài của một button (một ô)
+		public static int ChieuCaobtn = 30;
+
+		public static int ChieuRongBC = 25;		// Số ô ngang
+		public static int ChieuCaoBC = 19;		// Số ô dọc
+
+        public static int Sumbtn = ChieuCaoBC * (ChieuRongBC - 1);  //Tổng số ô
+	}
+}
---
+++ Game caro/caroupdate/TestCode/FormNhapthongTin.cs
@@ -0,0 +1,41 @@
+﻿using GameCaro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace GameCaro
+{
+    public partial class FormNhapthongTin : Form
+    {
+        public static string name1;
+        public static string name2;
+
+        public string Name11 { get => name1; set => name1 = value; }
+        public  string Name21 { get => name2; set => name2 = value; }
+
+        public FormNhapthongTin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Name11 = Name1.Text;
+            Name21 = Name2.Text;
+
+            Form1 f = new Form1();
+            f.Show();
+        }
+
+        private void FormNhapthongTin_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = this.Name1;
+        }
+
+    }
+}
